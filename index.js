e9e0454aeea5821a4d8618c19f784333b26ea961
gretzky <sfed617@gmail.com>
2018-10-29 19:16:42 -0400

    remove brill

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const brill = require("brill");
 const syllable = require("syllable");
-
-/**
- * isAdjective - checks whether or not a word is an adjective
- * @param {string} word - the word to check
- */
-const isAdjective = word => {
-  if (brill[word].includes("JJ")) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
 /**
  * switchY - checks if last letter of the word is 'y' and replaces it with an 'i'
@@ -31,20 +18,14 @@ const getSuffix = compare => (compare ? "er" : "est");
  * @param {boolean} compare
  */
 const cs = (word, compare) => {
-  // word must be an adjective
-  if (isAdjective(word)) {
-    // if word has 3 or more syllables or ends in 's', we prepend 'more' or 'most'
-    if (syllable(word) >= 3 || word.slice(-1) === "s") {
-      return compare ? "more " : "most " + word;
-    } else {
-      // otherwise, return the 'er' or 'est' suffix
-      return word.slice(-1) === "y"
-        ? switchY(word) + getSuffix(compare)
-        : word + getSuffix(compare);
-    }
+  // if word has 3 or more syllables or ends in 's', we prepend 'more' or 'most'
+  if (syllable(word) >= 3 || word.slice(-1) === "s") {
+    return compare ? "more " : "most " + word;
   } else {
-    console.error("Word must be an adjective.");
-    return false;
+    // otherwise, return the 'er' or 'est' suffix
+    return word.slice(-1) === "y"
+      ? switchY(word) + getSuffix(compare)
+      : word + getSuffix(compare);
   }
 };
 
